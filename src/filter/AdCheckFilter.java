7d9b944ba6589358614f6d5a64e6577abd792d52
min <hmy343@gmail.com>
2022-09-30 00:54:37 +0900

    login and admin check filter complete

--- src/filter/AdCheckFilter.java
+++ src/filter/AdCheckFilter.java
@@ -25,7 +25,7 @@ public class AdCheckFilter implements Filter {
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
-		adminBox = new String[] { "/admin-user", "adUserList.jsp"};
+		adminBox = new String[] { "/admin-user", "/adUserList.jsp", "/adRentalList.jsp", "/admin-rental"};
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -36,16 +36,16 @@ public class AdCheckFilter implements Filter {
 
 		// 경로에 대한 구분과 grade에 대한 구분이 필요함
 		// admin페이지의 경우 일반 사용자 접근 불가능
-		if (Arrays.asList(adminBox).contains(req.getServletPath())) {
-			if (!"Admin".equals(session.getAttribute("usGrade"))) {
-				res.sendRedirect("./main.do");
-				return;
-			}
-		} else {
-			if ("Admin".equals(session.getAttribute("usGrade"))) {
-				res.sendRedirect("./admin-user");
-				return;
-			}
+		if (Arrays.asList(adminBox).contains(req.getServletPath())
+				&& !"/logout".equals(req.getServletPath())
+				&& !"Admin".equals(session.getAttribute("usGrade")) ) {
+			res.sendRedirect("./main.do");
+			return;
+		} else if (!Arrays.asList(adminBox).contains(req.getServletPath())
+				&& "Admin".equals(session.getAttribute("usGrade"))
+				&& !"/logout".equals(req.getServletPath())) {
+			res.sendRedirect("./admin-user");
+			return;
 		}
 		chain.doFilter(request, response);
 	}
